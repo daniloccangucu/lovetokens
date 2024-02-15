6b0dde6a391d042ab59a7a0ff87f7375e161da75
Danilo Canguçu <daniloccangucu@gmail.com>
2024-02-15 15:14:21 +0200

    refactor Home component, added FeaturedLoveToken

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { useFetchFeaturedLoveTokensQuery } from "../../store/api";
 
-import huggingHands from "../../images/hands-hugging-heart.png";
 import { getErrorMessage } from "../../utils/apiUtils";
+import FeaturedLoveToken from "./FeaturedLoveToken";
 
 function Home() {
   const { data: featuredLoveTokens = [], error, isLoading } = useFetchFeaturedLoveTokensQuery();
@@ -28,22 +28,7 @@ function Home() {
         featuredLoveTokens.length === 0 ? (
           <h2>There are no featured Love Tokens to be displayed</h2>
         ) : (
-          <div key={currentIndex} className="flex flex-col items-center section-container--max-height">
-            <div className="px-4 text-6xl">
-              <p className="text--persian-pink">“I feel loved when you</p>
-              <p>
-                {currentLoveToken.phrase}
-                <span className="text--persian-pink">”</span>
-              </p>
-              <p className="italic text-base mt-4">
-                Love token created by {currentLoveToken.createdBy.userName}
-              </p>
-            </div>
-            <div>
-              <img className="object-contain h-50 w-60" src={huggingHands} alt="Illustration of hands hugging a heart" />
-            </div>
-          </div>
-
+          <FeaturedLoveToken {...currentLoveToken} />
         )
       )}
     </section>
