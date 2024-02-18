f2912dcc2b9bceeb6b3f8a04b615d5cd2c177555
Danilo Canguçu <daniloccangucu@gmail.com>
2024-02-18 10:34:30 +0200

    LoveArchive component refactored

--- src/components/lovearchive/LoveArchive.tsx
+++ src/components/lovearchive/LoveArchive.tsx
@@ -1,39 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import { useFetchLoveTokensQuery } from "../../store/api";
-import LoveTokenPreview from "./LoveTokenPreview";
-import DataLoader from "../../utils/DataLoader";
-import { LoveToken } from "../../models/LoveToken";
+import { RootState } from "../../models/Types";
+import PageHeader from "../headers/PageHeader";
 import CallToAction from "../calltoaction/CallToAction";
-import PageHeader from "../pageheader/PageHeader";
-import CategoryButton from "./CategoryButton";
+import CategoriesSection from "./CategoriesSection";
+import { toggleCategory } from "../../store/categoriesSlice";
+import LoveTokensPreviewSection from "./LoveTokensPreviewSection";
 
 function LoveTokens() {
-    const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
-    const [loveTokensToDisplay, setLoveTokensToDisplay] = useState<LoveToken[]>([]);
+    const selectedCategories = useSelector((state: RootState) => state.categories.selectedCategories);
+    const dispatch = useDispatch();
 
-    const { data: allLoveTokens = [], isLoading, error } = useFetchLoveTokensQuery();
+    const { refetch: refetchLoveTokens } = useFetchLoveTokensQuery(selectedCategories);
 
     const handleCategorySelect = (category: string) => {
-        setSelectedCategories((prevCategories) => {
-            if (prevCategories.includes(category)) {
-                return prevCategories.filter((c) => c !== category);
-            } else {
-                return [...prevCategories, category];
-            }
-        });
+        dispatch(toggleCategory(category));
     };
 
     useEffect(() => {
-        if (selectedCategories.length === 0) {
-            setLoveTokensToDisplay(allLoveTokens);
-        } else {
-            const filteredLoveTokens = allLoveTokens.filter((loveToken: LoveToken) =>
-                loveToken.labels.some((label) => selectedCategories.includes(label))
-            );
-            setLoveTokensToDisplay(filteredLoveTokens);
-        }
-    }, [selectedCategories, allLoveTokens]);
+        refetchLoveTokens()
+    }, [selectedCategories, refetchLoveTokens]);
 
     return (
         <section className="p-4">
@@ -42,31 +30,12 @@ function LoveTokens() {
                 subtitle="Explore our collection of Love Tokens"
             />
             <CallToAction />
-            <DataLoader
-                isLoading={isLoading}
-                error={error}
-                data={loveTokensToDisplay}
-                emptyMessage="There are no featured Love Tokens to be displayed"
-                render={() => (
-                    <>
-                        <h2 className="mt-4 mb-4 text-2xl text--persian-pink">I feel loved when you...</h2>
-                        <div>
-                            {["Affection", "Intimacy"].map((category) => (
-                                <CategoryButton
-                                    key={category}
-                                    category={category}
-                                    selected={selectedCategories.includes(category)}
-                                    onClick={() => handleCategorySelect(category)}
-                                />
-                            ))}
-                        </div>
-                        <div className="flex flex-wrap justify-center items-center">
-                            {loveTokensToDisplay.map((loveToken: LoveToken) => (
-                                <LoveTokenPreview key={loveToken.tokenNumber} {...loveToken} />
-                            ))}
-                        </div>
-                    </>
-                )}
+            <CategoriesSection
+                selectedCategories={selectedCategories}
+                handleCategorySelect={handleCategorySelect}
+            />
+            <LoveTokensPreviewSection
+                selectedCategories={selectedCategories} 
             />
         </section>
     )
