76eeb9b257e419429736e2e52a0bb53b44f18fd9
Danilo Canguçu <daniloccangucu@gmail.com>
2024-03-05 23:29:01 +0100

    CustomButton fix for dark mode

--- src/components/shared/CustomButton.tsx
+++ src/components/shared/CustomButton.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { ButtonProps } from '../../models/Types';
+import { useTheme } from '../../contexts/ThemeContext';
 
 const CustomButton: React.FC<ButtonProps> = ({ onClick, isLoading, label, loadingText }) => {
+    const { theme } = useTheme();
+    const themeStyles = theme === 'dark' ? 'bg-gray-900 hover:bg-gray-800 text-gray-200' : 'background--ce-soir hover:background--ce-soir:hover text-white';
+    console.log(theme)
+
     return (
         <button
-            className={`font-bold py-1 px-2 rounded mt-1 background--ce-soir hover:background--ce-soir:hover text-white text-xs ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
+            className={`font-bold py-1 px-2 rounded mt-1 text-xs
+                ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}
+                ${themeStyles}`
+            }
             onClick={onClick}
             disabled={isLoading}
         >
